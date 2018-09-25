4d9c29f5f0481df71df6e313f3e43d7c5b13f1bd
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-09-25 19:30:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,9 +5,14 @@ class App < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
-end
 
   get '/' do
     erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
   end
 end
